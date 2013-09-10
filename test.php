69005e9aee7fb3c0bdb8726a735097c400f65e64
small-killer <small-killer@centrum.sk>
2013-09-10 16:20:41 +0200

    Update TEST files

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-echo('<link rel="stylesheet" href="status.css">'."\r\n");
+echo('<link rel="stylesheet" href="status.css"/>'."\r\n");
 
 require_once("functions.php");
 status(2, "37.157.196.35", 27960, 0, 5);
