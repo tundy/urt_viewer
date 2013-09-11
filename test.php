b6a7eb32df3f4dd663748feae72efe9f5e48ab10
small-killer <small-killer@centrum.sk>
2013-09-11 17:09:38 +0200

    Update TEST

--- test.php
+++ test.php
@@ -3,6 +3,7 @@
 echo('<link rel="stylesheet" href="status.css"/>'."\r\n");
 
 require_once("functions.php");
-status(2, "37.157.196.35", 27960, 0, 5);
+status(1, "37.157.196.35", 27960, 0, 0);
+status(1, "server.urtsk.eu", 27960, 0, 0);
 
 ?>
